48cd1e511cef385277c75f93e49c5a152644c2db
lavawulence <debouangabriceamour@gmail.com>
2026-05-27 13:50:03 +0100

    Create src/config/admin.js

---
+++ src/config/admin.js
@@ -0,0 +1,13 @@
+// Configuration des administrateurs
+const ADMINS = [
+    '242044106402',  // Votre numéro (sans le @s.whatsapp.net)
+    // Ajoutez d'autres admins ici si besoin
+];
+
+// Points illimités pour les admins
+const UNLIMITED_POINTS = Infinity;
+
+// Préfixe des commandes
+const COMMAND_PREFIX = '$';
+
+module.exports = { ADMINS, UNLIMITED_POINTS, COMMAND_PREFIX };
