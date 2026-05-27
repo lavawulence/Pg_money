380851da3feefb743624a5c6812894ae50f37d11
lavawulence <debouangabriceamour@gmail.com>
2026-05-27 14:31:57 +0100

    Create src/config/secret.js

---
+++ src/config/secret.js
@@ -0,0 +1,11 @@
+// Code secret pour la connexion admin
+const SECRET_CODE = 'LAVAWULE';
+
+// Configuration des images
+const IMAGES = {
+    botAvatar: './public/images/bot-avatar.jpg',
+    botBackground: './public/images/bot-bg.jpg',
+    botLogo: './public/images/bot-logo.png'
+};
+
+module.exports = { SECRET_CODE, IMAGES };
